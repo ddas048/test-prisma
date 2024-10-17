dfac9346de88c0f63f5659b405576862a4731d83
dd <web.chinawholesale@gmail.com>
2024-10-18 01:50:55 +0600

    error handel in create post

--- src/actions/post.action.ts
+++ src/actions/post.action.ts
@@ -1,24 +1,33 @@
 "use server";
 
 import prisma from "@/lib/db";
+import { Prisma } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 
 export async function createPost(formData: FormData) {
-  await prisma.post.create({
-    data: {
-      title: formData.get("title") as string,
-      content: formData.get("content") as string,
-      slug: (formData.get("title") as string)
-        .replace(/\s+/g, "-")
-        .toLowerCase(),
-      published: false,
-      author: {
-        connect: {
-          email: "a@a.io",
+  try {
+    await prisma.post.create({
+      data: {
+        title: formData.get("title") as string,
+        content: formData.get("content") as string,
+        slug: (formData.get("title") as string)
+          .replace(/\s+/g, "-")
+          .toLowerCase(),
+        published: false,
+        author: {
+          connect: {
+            email: "a@a.io",
+          },
         },
       },
-    },
-  });
+    });
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
+        console.log("Duplicate title slug");
+      }
+    }
+  }
 
   revalidatePath("/");
 }
