441ca2a5dfa94d91b59449f6b562d7aaa3e74852
dd <web.chinawholesale@gmail.com>
2024-10-18 00:25:17 +0600

    remove return from actions

--- src/actions/post.action.ts
+++ src/actions/post.action.ts
@@ -1,35 +1,42 @@
 "use server";
 
 import prisma from "@/lib/db";
+import { revalidatePath } from "next/cache";
 
 export async function createPost(formData: FormData) {
-  return await prisma.post.create({
+  await prisma.post.create({
     data: {
       title: formData.get("title") as string,
       content: formData.get("content") as string,
-      slug: (formData.get("title") as string).replace(/\s+/g, "-").toLowerCase(),
+      slug: (formData.get("title") as string)
+        .replace(/\s+/g, "-")
+        .toLowerCase(),
       published: false,
     },
   });
+
+  revalidatePath("/");
 }
 
 export async function editPost(formData: FormData, id: string) {
-  return await prisma.post.update({
+  await prisma.post.update({
     where: {
       id,
     },
     data: {
       title: formData.get("title") as string,
       content: formData.get("content") as string,
-      slug: (formData.get("title") as string).replace(/\s+/g, "-").toLowerCase(),
+      slug: (formData.get("title") as string)
+        .replace(/\s+/g, "-")
+        .toLowerCase(),
     },
   });
 }
 
 export async function deletePost(id: string) {
-  return await prisma.post.delete({
+  await prisma.post.delete({
     where: {
-      id
-    }
+      id,
+    },
   });
 }
