f45bbd2a9d0859ef9f3bf10753f32eed704656f4
dd <web.chinawholesale@gmail.com>
2024-10-18 18:41:45 +0600

    nextjs data cashe

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,19 +1,25 @@
 import prisma from "@/lib/db";
+import { unstable_cache as cashe } from "next/cache";
+
+const getCahedPost = cashe((slug: string) => {
+  return prisma.post.findUnique({
+    where: {
+      slug,
+    },
+  });
+});
 
 export default async function PostPage({
   params,
 }: {
   params: { slug: string };
 }) {
-  const post = await prisma.post.findUnique({
-    where: {
-      slug: params.slug,
-    },
-  });
+  const post = await getCahedPost(params.slug);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-y-5  pt-25 text-center">
+        <a href="/">👈 Back</a>
         <h1 className="text-3xl font-semibold">{post?.title}</h1>
         <p className="">{post?.content}</p>
       </main>
